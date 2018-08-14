632c48be6830d1b838652e150ea8c1f73a5d615e
PacteraKun <kun.li24@pactera.com>
2018-08-14 15:19:07 -0700

    Set Mode_mask to False and mode_fpn to True

--- examples/CascadeRCNN/config.py
+++ examples/CascadeRCNN/config.py
@@ -56,8 +56,8 @@ _C = config     # short alias to avoid coding
 
 # mode flags ---------------------
 _C.TRAINER = 'replicated'  # options: 'horovod', 'replicated'
-_C.MODE_MASK = True        # FasterRCNN or MaskRCNN
-_C.MODE_FPN = False
+_C.MODE_MASK = False        # FasterRCNN or MaskRCNN
+_C.MODE_FPN = True
 
 # dataset -----------------------
 _C.DATA.BASEDIR = '/path/to/your/COCO/DIR'
